87f9b272606f10391e4adc258b8f786bc7b6dcbf
aceDavon <makojidavid@gmail.com>
2025-04-07 15:28:03 +0100

    Allow all origins for CORS

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,26 @@
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
+import cors from "cors"
 
 import userRoutes from "./routes/user.route"
 
 dotenv.config()
 const app = express()
+app.use(
+  cors({
+    origin: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    credentials: true,
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "Accept",
+    ],
+    exposedHeaders: ["Content-Range", "X-Content-Range"],
+  })
+)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
